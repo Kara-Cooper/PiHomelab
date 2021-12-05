f66354cb8f4e41de3dc32512c1000c938086105e
Kara <kara.cooper@hotmail.com>
2021-12-04 23:44:38 -0800

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,24 @@
+which bash
+nano bash.sh
+chmod +x bash.sh
+./bash.sh
+
+#! /usr/bin/bash
+ apt update -y &&
+ apt upgrade -y &&
+ apt autoremove -y &&
+ apt-get update -y &&
+ apt install exfat-fuse -y &&
+ apt install fuse -y &&
+ apt install ntfs-3g -y &&
+ apt install openssh-server -y &&
+ apt-get install samba samba-common-bin -y &&
+ apt-get install apt-transport-https -y &&
+ apt-get install aptitude -y &&
+ apt-get install ca-certificates -y &&
+ apt-get install curl  -y &&
+ apt-get install gnupg  -y &&
+ apt-get install lsb-release -y &&
+ apt update -y &&
+ apt upgrade -y &&
+ apt autoremove -y
