831117abdb76653115af88bb59cae1485624700b
Kara <kara.cooper@hotmail.com>
2021-12-05 16:38:59 -0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,24 +1,30 @@
-which bash
-nano bash.sh
-chmod +x bash.sh
-./bash.sh
-
 #! /usr/bin/bash
  apt update -y &&
  apt upgrade -y &&
  apt autoremove -y &&
- apt-get update -y &&
- apt install exfat-fuse -y &&
- apt install fuse -y &&
+ apt install openssh-server -y && 
+ apt install exfat-fuse -y && 
+ apt install fuse -y && 
  apt install ntfs-3g -y &&
- apt install openssh-server -y &&
- apt-get install samba samba-common-bin -y &&
- apt-get install apt-transport-https -y &&
- apt-get install aptitude -y &&
+ apt-get install samba samba-common-bin -y && 
+ apt-get install apt-transport-https -y && 
+ apt-get install aptitude -y && 
+ apt install net-tools -y &&
  apt-get install ca-certificates -y &&
- apt-get install curl  -y &&
- apt-get install gnupg  -y &&
+ apt-get install curl -y &&
+ apt-get install gnupg -y &&
  apt-get install lsb-release -y &&
  apt update -y &&
- apt upgrade -y &&
- apt autoremove -y
+ apt upgrade -y && 
+ apt autoremove -y &&
+ apt-get remove docker docker-engine docker.io containerd runc -y &&
+ apt-get update -y &&
+ curl -fsSL https://download.docker.com/linux/debian/gpg |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
+ apt-get update -y &&
+ apt-get install docker-ce docker-ce-cli containerd.io -y &&
+ systemctl enable docker && 
+ systemctl start docker &&
+ docker run -d --name="portainer" --restart on-failure -p 9000:9000 -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
