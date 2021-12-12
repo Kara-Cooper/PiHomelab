f87c8723ddc0ab385855ddfcf58efde1da190ff3
Kara <kara.cooper@hotmail.com>
2021-12-11 23:50:00 -0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -21,10 +21,10 @@
  
  apt-get remove docker docker-engine docker.io containerd runc -y &&
  apt-get update -y &&
- curl -fsSL https://download.docker.com/linux/debian/gpg |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ curl -fsSL https://download.docker.com/linux/debian/gpg |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg &&
  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null &&
  apt-get update -y &&
  apt-get install docker-ce docker-ce-cli containerd.io -y &&
  systemctl enable docker && 
